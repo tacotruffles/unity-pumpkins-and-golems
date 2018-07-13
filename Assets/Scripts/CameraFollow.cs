b9ffb818c752beffee2046c88d339d179fa635d1
John Dawes <jdawes@bluebitmedia.com>
2018-07-12 21:36:34 -0600

    Golums and colliders

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Transform pumpkinPos;
+
+	private float zDistance = 7f;
+	private float yDistance = 4f;
+
+	void Awake () {
+		// Get the Pumpkin Position
+		pumpkinPos = GameObject.Find ("Pumpkin").transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 temp = transform.position;
+		temp.y = pumpkinPos.position.y + yDistance;
+		temp.z = pumpkinPos.position.z - zDistance;
+
+		transform.position = temp;
+	}
+
+	void FixedUpdate() {
+
+	}
+} // end class - CameraFollow
--- Assets/Scripts/Pumpkin.cs
+++ Assets/Scripts/Pumpkin.cs
@@ -20,19 +20,30 @@ public class Pumpkin : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// If we fall off the front of the floor. Stop the game
+		if(transform.position.y < -10) {
+			Time.timeScale = 0f;
+		}
 	}
 
 	// Called every several frames
 	void FixedUpdate() {
+		// Keep moving the pumpkin with arrow keys
 		MovePumpkin ();	
 	}
 
-
+	// Pumpkin motion
 	void MovePumpkin () {
 		float h = Input.GetAxis ("Horizontal");
 		float v = Input.GetAxis ("Vertical");
 
 		myBody.AddForce (new Vector3(h * moveForce, 0f, v * moveForce));
+	}
+
+	// If we hit a Golem or Go Through the Pearly Gates...Game Over!
+	void OnTriggerEnter(Collider target) {
+		if(target.tag == "Golem" || target.tag == "Gate") {
+			Time.timeScale = 0f;
+		}
 	}
 } // class Pumpkin
